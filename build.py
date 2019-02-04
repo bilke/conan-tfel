f7f2259e1e5a02abf7821bf09558536ba68fe0b9
Lars Bilke <lars.bilke@ufz.de>
2019-02-04 10:53:32 +0100

    Added azure-pipelines.yml

---
+++ build.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conan.packager import ConanMultiPackager
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(archs = ["x86_64"])
+    builder.add_common_builds(pure_c=False)
+    builder.run()
