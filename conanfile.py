785f087d68c208638431d8813ab2d32716cb56f7
Lars Bilke <lars.bilke@ufz.de>
2020-07-03 08:08:35 +0200

    tfel 3.3.0

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class TfelConan(ConanFile):
     name = "tfel"
-    version = "3.2.0"
+    version = "3.3.0"
     license = "MIT"
     author = "Lars Bilke, lars.bilke@ufz.de"
     url = "https://github.com/bilke/tfel"
@@ -16,9 +16,9 @@ class TfelConan(ConanFile):
     generators = "cmake"
 
     def source(self):
-        tools.get("https://ogsstorage.blob.core.windows.net/tmp/tfel-{0}.tar.bz2"
+        tools.get("https://github.com/thelfer/tfel/archive/TFEL-{0}.tar.gz"
                   .format(self.version))
-        extracted_dir = self.name + "-" + self.version
+        extracted_dir = self.name + "-" + self.name.upper() + "-" + self.version
         os.rename(extracted_dir, "tfel")
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
@@ -27,26 +27,30 @@ class TfelConan(ConanFile):
             tools.replace_in_file("tfel/CMakeLists.txt", "set(PACKAGE_BUGREPORT \"tfel-contact@cea.fr\")",
                                   '''set(PACKAGE_BUGREPORT "tfel-contact@cea.fr")
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+conan_basic_setup(KEEP_RPATHS)
+''')
+            tools.replace_in_file("tfel/CMakeLists.txt", "set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)",
+                "set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)")
+            tools.replace_in_file("tfel/CMakeLists.txt", "set(CMAKE_INSTALL_RPATH \"${CMAKE_INSTALL_PREFIX}/lib\")",
+                "set(CMAKE_INSTALL_RPATH \"@executable_path/../lib\")")
+
 
         # https://sourceforge.net/p/tfel/discussion/installation/thread/9a05c751cf
         tools.replace_in_file('tfel/src/System/SignalManager.cxx', '::sigfillset(&nSigSet);',
                               'sigfillset(&nSigSet);')
 
-    def build(self):
+    def configure_cmake(self):
         cmake = CMake(self)
         cmake.configure(source_folder="tfel")
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
         cmake.build()
-        # cmake.build(target='check')
-        cmake.install()
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
